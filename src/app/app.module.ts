57d27bdc444ac708484395270ed0bd87105a671b
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-11-07 11:17:08 -0800

    updated files and created maths.json for mats-quiz app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
