84c24fb6fc703aa6b840bd2f2569482c3e95b634
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-11-12 14:12:28 -0800

    Added results view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,34 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MatCardModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    MatCardModule,
+    BrowserAnimationsModule,
     HttpClientModule
   ],
   providers: [],
